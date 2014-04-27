80df922b19940732165f25254a93b5a78581169c
Vicyorus <andres.chaves.diaz@gmail.com>
2014-04-27 08:15:32 -0600

    Reverting to last stable version
    Reverting due to problems with v. 1.5.2 (BadStatusLine)

--- chatbot.py
+++ chatbot.py
@@ -4,16 +4,16 @@ import time
 import json
 import urllib
 import threading
+import cookielib
+import urllib2
 
-import requests
-
-__version__ = '1.5.2b'
+__version__ = '1.4.6b'
 
 class Event(object):
-	def __init__(self, connection):
+        def __init__(self, connection):
 		self.connection = self.parse(connection)
 
-	def parse(self, connection):
+        def parse(self, connection):
 		returned = {}
 		if connection["event"] == "join":
 			connection["data"] = json.loads(connection["data"])
@@ -113,42 +113,47 @@ class Event(object):
 		else:
 			return False
 
-#class PrivateMessage(ChatBot):
-#	def __init__(self, username, password):
-#		ChatBot.__init__(self)
-#		self.c = Client(username, password, wiki)
-#		self.changed = False
-#		self.message = ""
-#
-#	def send(self, message):
-#		self.changed = True
-#		self.message = message
-#
-#	def on_message(self, c, e):
-#		if self.changed:
-#			c.send(c, message)
-#			self.changed = False
-
-
-
 class Client(object):
-	def __init__(self, username, password, site):
+	def __init__(self, username, password, site, priv=None, key=None):
 		self.username = username
 		self.password = password
-		self.session = requests.session()
-		self.wiki = site.rstrip('/')
+	        self.wikiname = site
+	        self.api_path = self.wikiname + "api.php"
+		self.wiki_path = self.wikiname + "wikia.php?controller=Chat&format=json"
+
+		#Cookie jar
+		self.cookie_jar = cookielib.CookieJar()
+		
+		#HTTP opener
+		self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie_jar))
+		self.opener.add_headers = [("User-Agent", "Wikibot/LMR-&-V")]
+		
+		#Basic operations
 		self.__login(username, password)
-		data = self.__wikia_request(controller="Chat", format="json")
+		data = self.__wikia_request()
 		self.settings = {}
-		self.settings['chatkey'] = data['chatkey']
+		
+		if key:
+			self.settings["chatkey"] = key
+		else:
+			self.settings['chatkey'] = data['chatkey']
+			
 		self.settings["port"] = data["nodePort"]
 		self.settings["host"] = data["nodeHostname"]
-		self.settings["room"] = data["roomId"]
+		
+		if priv:
+			self.settings["room"] = self.__private_message_id(priv)
+		else:
+			self.settings["room"] = data["roomId"]
+			
 		self.settings["chatmod"] = data["isChatMod"]
-		self.settings["session"] = self.__get_session(self.settings)
 		self.xhr = self.__initialize(self.settings)
 		self.priv_dict = {}
-
+		self.xhr_url = "http://" + self.settings["host"] + ":" + self.settings["port"] + "/socket.io/1/xhr-polling/" + \
+		                self.xhr + "?name=" + self.username + "&key=" + self.settings["chatkey"] + "&roomId=" + \
+		                str(self.settings["room"]) + "&t="
+	        self.connection()
+			    
 	def __login(self, username, password):
 		login_data = {
 			'action': 'login',
@@ -156,198 +161,41 @@ class Client(object):
 			'lgpassword': password,
 			'format':'json'}
 		data = urllib.urlencode(login_data)
-		response = self.session.post(self.wiki + "/api.php",data=login_data)
-		content = json.loads(response.content)
+		response = self.opener.open(self.api_path, data)
+		content = json.loads(response.read())
 		login_data['lgtoken'] = content['login']['token']
 		data = urllib.urlencode(login_data)
-		response = self.session.post(self.wiki + "/api.php",data=login_data)
-		content = json.loads(response.content)
+		response = self.opener.open(self.api_path, data)
+		content = json.loads(response.read())
 		if content['login']['result'] != 'Success':
 			print 'Couldn\'t log in: Quitting.'
 			sys.exit(1)
 
-	def __wikia_request(self, **kwargs):
-		request = {}
-		for karg in kwargs:
-			request[karg] = kwargs[karg]
-		response = self.session.get(self.wiki + "/wikia.php",params=request)
-		content = json.loads(response.content)
+	def __wikia_request(self):
+		response = self.opener.open(self.wiki_path)
+		content = json.loads(response.read())
 		return content
 
-	def __get_session(self, settings):
-		data = self.session.get("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/?name=" +
-			self.username + "&key=" + settings["chatkey"]
-			+ "&roomId=" + str(settings["room"]) + "&jsonp=1")
-		return data.text
-
 	def __initialize(self, settings):
-		data = self.session.get("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/?name=" +
+		data = self.opener.open("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/?name=" +
 			self.username + "&key=" + settings["chatkey"]
 			+ "&roomId=" + str(settings["room"]) + "&jsonp=1")
-		return data.content
-
-	def __timestamp(self):
-		unix = time.time()
-		return str(round(unix, 0))
-
-	def __get_code(self, xhr):
 		regex = re.compile(r'io\..*?\(\"(.*?):.*?\"\);')
-		array = regex.findall(xhr)
+		array = regex.findall(data.read())
 		if array:
 			return array[0]
 		else:
 			return None
 
-	def __send(self, settings, xhr, message):
-		xhr_polling = self.__get_code(xhr)
-		extras =  json.dumps({'attrs': {'msgType': 'chat', 'text': message}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" + 
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name':'message','args': [extras]}))
-		return
-
-	def __go_away(self, settings, xhr):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType':'command','command':'setstatus','statusState':'away'}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" + 
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name': 'message','args': [extras]}))
-		return
-
-	def __come_back(self, settings, xhr):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType':'command','command':'setstatus','statusState':'here'}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" +
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name': 'message','args': [extras]}))
-		return
-
-	def __kick_user(self, settings, xhr, user):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType':'command','command':'kick','userToKick':user}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" + 
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name': 'message', 'args': [extras]}))
-		return
-
-	def __ban_user(self, settings, xhr, user, time, reason):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType':'command','command':'ban','userToBan':user,
-			'time':time,'reason':reason}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" + 
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name': 'message','args': [extras]}))
-		return
-
-	def __end_ban(self, settings, xhr, user, reason):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType':'command', 'command':'ban', 'userToBan':user,
-			'time':'0', 'reason':reason}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" + xhr_polling + "?name=" +
-			self.username + "&key=" + 
-			settings['chatkey'] + "&roomId=" + str(settings['room']) + "&t=" + self.__timestamp(),
-			'5:::' + json.dumps({'name': 'message', 'args': [extras]}))
-		return
-
-	def __give_chatmod(self, settings, xhr, user):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType': 'command','command': 'givechatmod', 'userToPromote': user}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" +
-								xhr_polling + "?name=" + self.username + "&key=" + 
-								settings['chatkey'] + "&roomId=" + str(settings['room']) +
-								"&t=" + self.__timestamp(),
-								'5:::' + json.dumps({'name': 'message',
-								'args': [extras]}))
-		return
-
-	def __disconnect(self, settings, xhr, nodisconnect=False):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType': 'command','command': 'logout'}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" +
-								xhr_polling + "?name=" + self.username + "&key=" + 
-								settings['chatkey'] + "&roomId=" + str(settings['room']) +
-								"&t=" + self.__timestamp(),
-								'5:::' + json.dumps({'name': 'message',
-								'args': [extras]}))
-		if not nodisconnect:
-			sys.exit(0)
-		return
-
-	def __private_message_id(self, user):
-		data = self.session.post(self.wiki + "/index.php?action=ajax&rs=ChatAjax&method=getPrivateRoomId&users=[\"" +
-									user + "\",\"" + self.username + "\"]")
-		content = json.loads(data.content)
-		roomid = content['id']
-		return roomid
-
-	def __open_private_chat(self, settings, xhr, user, room):
-		xhr_polling = self.__get_code(xhr)
-		extras = json.dumps({'attrs': {'msgType': 'command','command': 'openprivate', 'roomId':str(room),
-										'users': [user]}})
-		data = self.session.post("http://" + settings["host"] + ":" + settings["port"] + "/socket.io/1/xhr-polling/" +
-								xhr_polling + "?name=" + self.username + "&key=" + 
-								settings['chatkey'] + "&roomId=" + str(settings["room"]) +
-								"&t=" + self.__timestamp(),
-								'5:::' + json.dumps({'name': 'message',
-								'args': [extras]}))
-		return
-
-	def __private_message(self, settings, user, message):
-		if self.priv_dict.has_key(user):
-			xhr = self.__initialize(settings)
-			self.__open_private_chat(settings, xhr, user, self.__private_message_id(user))
-			self.priv_dict[user].send(message, xhr)
-		else:
-			self.priv_dict[user] = PrivateMessage(user, self.username,
-												self.password, self.wiki, settings['chatkey'])
-			self.__private_message(settings, user, message)
-
-	def send(self, message, xhr=None):
-		if xhr:
-			self.__send(self.settings, xhr, message)
-		else:
-			self.__send(self.settings, self.xhr, message)
-
-	def private_message(self, user, message):
-		self.__private_message(self.settings, user, message)
-
-	def go_away(self):
-		self.__go_away(self.settings, self.xhr)
-
-	def come_back(self):
-		self.__come_back(self.settings, self.xhr)
-
-	def kick_user(self, user):
-		self.__kick_user(self.settings, self.xhr, user)
-
-	def ban_user(self, user, time=3600, reason='Misbehaving in chat'):
-		self.__ban_user(self.settings, self.xhr, user, time, reason)
-
-	def end_ban(self, user, reason='Ending chat ban'):
-		self.__end_ban(self.settings, self.xhr, user, reason)
-
-	def give_chatmod(self, user):
-		self.__give_chatmod(self.settings, self.xhr, user, reason)
-
-	def disconnect(self, nodisconnect=False):
-		self.__disconnect(self.settings, self.xhr, nodisconnect=nodisconnect)
-
-	def __connection(self, settings, xhr):
-		xhr_polling = self.__get_code(xhr)
+	def __timestamp(self):
+		unix = time.time()
+		return str(round(unix, 0))
+	    
+	def __connection(self):
 		time = self.__timestamp()
-		data = self.session.get("http://" + settings["host"] + ":" +
-								settings["port"] +
-								"/socket.io/1/xhr-polling/" + xhr_polling
-								+ "?name=" + self.username + "&key=" +
-								settings['chatkey'] + "&roomId=" +
-								str(settings['room']) + "&t=" +
-								time)
-		content = re.findall(r'.:::(.*)', data.content.decode('utf8'))
+		print self.xhr_url + time
+		data = self.opener.open(self.xhr_url + time)
+		content = re.findall(r'.:::(.*)', data.read().decode('utf8'))
 		if content:
 			try:
 				loads = json.loads(content[0])
@@ -358,9 +206,86 @@ class Client(object):
 			return None
 
 	def connection(self):
-		var = self.__connection(self.settings, self.xhr)
+		var = self.__connection()
 		return var
 
+	def __send(self, message):
+		extras =  json.dumps({'attrs': {'msgType': 'chat', 'text': message}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(), 
+			  '5:::' + json.dumps({'name':'message','args': [extras]}))
+		return
+
+	def __go_away(self):
+		extras = json.dumps({'attrs': {'msgType':'command','command':'setstatus','statusState':'away'}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+			'5:::' + json.dumps({'name': 'message','args': [extras]}))
+		return
+
+	def __come_back(self):
+		extras = json.dumps({'attrs': {'msgType':'command','command':'setstatus','statusState':'here'}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+			'5:::' + json.dumps({'name': 'message','args': [extras]}))
+		return
+
+	def __kick_user(self, user):
+		extras = json.dumps({'attrs': {'msgType':'command','command':'kick','userToKick':user}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+			'5:::' + json.dumps({'name': 'message', 'args': [extras]}))
+		return
+
+	def __ban_user(self, user, time, reason):
+		extras = json.dumps({'attrs': {'msgType':'command','command':'ban','userToBan':user,
+			'time':time,'reason':reason}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+			'5:::' + json.dumps({'name': 'message','args': [extras]}))
+		return
+
+	def __end_ban(self, user, reason):
+		extras = json.dumps({'attrs': {'msgType':'command', 'command':'ban', 'userToBan':user,
+			'time':'0', 'reason':reason}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+			'5:::' + json.dumps({'name': 'message', 'args': [extras]}))
+		return
+
+	def __give_chatmod(self, user):
+		extras = json.dumps({'attrs': {'msgType': 'command','command': 'givechatmod', 'userToPromote': user}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+								'5:::' + json.dumps({'name': 'message',
+								'args': [extras]}))
+		return
+
+	def __disconnect(self, nodisconnect=False):
+		extras = json.dumps({'attrs': {'msgType': 'command','command': 'logout'}})
+		data = self.opener.open(self.xhr_url + self.__timestamp(),
+								'5:::' + json.dumps({'name': 'message',
+								'args': [extras]}))
+		if not nodisconnect:
+			sys.exit(0)
+		return
+
+	def send(self, message):
+		self.__send(message)
+
+	def go_away(self):
+		self.__go_away()
+
+	def come_back(self):
+		self.__come_back()
+
+	def kick_user(self, user):
+		self.__kick_user(user)
+
+	def ban_user(self, user, time=3600, reason='Misbehaving in chat'):
+		self.__ban_user(user, time, reason)
+
+	def end_ban(self, user, reason='Ending chat ban'):
+		self.__end_ban(user, reason)
+
+	def give_chatmod(self, user):
+		self.__give_chatmod(user, reason)
+
+	def disconnect(self, nodisconnect=False):
+		self.__disconnect(nodisconnect=nodisconnect)
 
 class ChatBot(threading.Thread):
 	def __init__(self, username, password, site):
@@ -396,9 +321,6 @@ class ChatBot(threading.Thread):
 	def on_chatmod(self, c, e):
 		pass
 
-	def on_idle(self, c, e):
-		pass
-
 	def run(self):
 		in_chat = 0
 		while 1:
@@ -426,8 +348,6 @@ class ChatBot(threading.Thread):
 					self.on_chatmod(self.c, e)
 				elif connect["event"] == "chat:add":
 					self.on_message(self.c, e)
-				else:
-					self.on_idle(self.c, e)
 
 if __name__ == '__main__':
 	print("""This file isn't exucutable.
